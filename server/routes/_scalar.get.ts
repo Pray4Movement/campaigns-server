47045927a9e411a18e88712d65da38fcbe14b57c
corsacca <corsacca@gmail.com>
2025-11-19 21:29:07 +0100

    Setup scalar for api docs

---
+++ server/routes/_scalar.get.ts
@@ -0,0 +1,18 @@
+export default defineEventHandler(() => {
+  return `<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Prayer Tools API - Scalar</title>
+  </head>
+  <body>
+    <script
+      id="api-reference"
+      data-url="/_openapi.yaml"
+      data-configuration='{"theme":"purple"}'
+    ></script>
+    <script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
+  </body>
+</html>`
+})
