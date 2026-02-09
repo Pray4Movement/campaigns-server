1b89caa625a7862932e338b87bb720cd1e25ac7d
corsacca <corsacca@gmail.com>
2026-02-09 13:45:02 -0600

    Re-enable pasting formatted content

--- app/components/RichTextEditor.vue
+++ app/components/RichTextEditor.vue
@@ -10,6 +10,7 @@ import Typography from '@tiptap/extension-typography'
 import Subscript from '@tiptap/extension-subscript'
 import Superscript from '@tiptap/extension-superscript'
 import Youtube from '@tiptap/extension-youtube'
+import { DOMParser as ProseMirrorDOMParser } from '@tiptap/pm/model'
 import { ImageUploadExtension } from '~/utils/imageUploadExtension'
 import { Spacer } from '~/extensions/spacer'
 import { Vimeo } from '~/extensions/vimeo'
@@ -31,6 +32,166 @@ const emit = defineEmits<{
 
 const { showError } = useModal()
 const { createCustomHandlers } = useEditorHandlers()
+
+/**
+ * Normalize pasted HTML to ensure TipTap can parse it correctly.
+ * Strips framework-specific attributes and normalizes elements.
+ */
+const transformPastedHTML = (html: string): string => {
+  const container = document.createElement('div')
+  container.innerHTML = html
+
+  container.querySelectorAll('meta, style, script, noscript').forEach((el) => el.remove())
+
+  container.querySelectorAll('.wp-block-spacer, [aria-hidden="true"]').forEach((el) => el.remove())
+
+  container.querySelectorAll('.section-header').forEach((div) => {
+    const heading = div.querySelector('h1, h2, h3, h4, h5, h6')
+    if (heading) {
+      div.replaceWith(heading)
+    }
+  })
+
+  container.querySelectorAll('figure').forEach((figure) => {
+    const img = figure.querySelector('img')
+    if (img) {
+      figure.replaceWith(img)
+    }
+  })
+
+  container.querySelectorAll('mark').forEach((mark) => {
+    const span = document.createElement('span')
+    span.innerHTML = mark.innerHTML
+    mark.replaceWith(span)
+  })
+
+  container.querySelectorAll('.gb-notice-title, .gb-notice-text, .wp-block-genesis-blocks-gb-notice').forEach((div) => {
+    const fragment = document.createDocumentFragment()
+    while (div.firstChild) {
+      fragment.appendChild(div.firstChild)
+    }
+    div.replaceWith(fragment)
+  })
+
+  container.querySelectorAll('div.wp-block-image, div[class^="wp-block"]').forEach((div) => {
+    const hasBlockContent = div.querySelector('p, h1, h2, h3, h4, h5, h6, ul, ol, img, blockquote')
+    if (hasBlockContent || div.children.length === 0) {
+      const fragment = document.createDocumentFragment()
+      while (div.firstChild) {
+        fragment.appendChild(div.firstChild)
+      }
+      div.replaceWith(fragment)
+    }
+  })
+
+  const allElements = container.querySelectorAll('*')
+  allElements.forEach((el) => {
+    const attrsToRemove: string[] = []
+    for (const attr of el.attributes) {
+      if (
+        attr.name.startsWith('data-v-') ||
+        attr.name.startsWith('data-react') ||
+        attr.name.startsWith('data-ng-') ||
+        attr.name.startsWith('ng-') ||
+        attr.name.startsWith('_ngcontent') ||
+        attr.name.startsWith('_nghost')
+      ) {
+        attrsToRemove.push(attr.name)
+      }
+    }
+    attrsToRemove.forEach((attr) => el.removeAttribute(attr))
+
+    if (el.tagName === 'SPAN') {
+      const style = (el as HTMLElement).style
+      if (style.fontWeight === 'bold' || style.fontWeight === '700' || style.fontWeight === '600') {
+        const strong = document.createElement('strong')
+        strong.innerHTML = el.innerHTML
+        el.replaceWith(strong)
+      } else if (style.fontStyle === 'italic') {
+        const em = document.createElement('em')
+        em.innerHTML = el.innerHTML
+        el.replaceWith(em)
+      }
+    }
+
+    if (el.tagName === 'B') {
+      const strong = document.createElement('strong')
+      strong.innerHTML = el.innerHTML
+      el.replaceWith(strong)
+    }
+
+    if (el.tagName === 'I' && !el.classList.contains('icon')) {
+      const em = document.createElement('em')
+      em.innerHTML = el.innerHTML
+      el.replaceWith(em)
+    }
+  })
+
+  container.querySelectorAll('h1, h2, h3, h4, h5, h6').forEach((heading) => {
+    heading.removeAttribute('class')
+
+    if (['H4', 'H5', 'H6'].includes(heading.tagName)) {
+      const h3 = document.createElement('h3')
+      h3.innerHTML = heading.innerHTML
+      heading.replaceWith(h3)
+    }
+  })
+
+  container.querySelectorAll('ul, ol, li').forEach((el) => {
+    el.removeAttribute('class')
+    el.removeAttribute('style')
+  })
+
+  container.querySelectorAll('a').forEach((link) => {
+    const href = link.getAttribute('href')
+    while (link.attributes.length > 0) {
+      const attr = link.attributes[0]
+      if (attr) link.removeAttribute(attr.name)
+    }
+    if (href) {
+      link.setAttribute('href', href)
+    }
+  })
+
+  container.querySelectorAll('p').forEach((p) => {
+    p.removeAttribute('class')
+    const style = p.getAttribute('style')
+    if (style && !style.includes('text-align')) {
+      p.removeAttribute('style')
+    }
+  })
+
+  container.querySelectorAll('span:empty, div:empty').forEach((el) => {
+    if (!el.hasChildNodes()) {
+      el.remove()
+    }
+  })
+
+  return container.innerHTML
+}
+
+function handlePaste(view: any, event: ClipboardEvent) {
+  const clipboardData = event.clipboardData
+  if (!clipboardData) return false
+
+  const html = clipboardData.getData('text/html')
+  if (html) {
+    const cleanedHTML = transformPastedHTML(html)
+
+    const { state, dispatch } = view
+    const parser = ProseMirrorDOMParser.fromSchema(state.schema)
+    const container = document.createElement('div')
+    container.innerHTML = cleanedHTML
+
+    const slice = parser.parseSlice(container)
+    const tr = state.tr.replaceSelection(slice)
+    dispatch(tr)
+
+    return true
+  }
+
+  return false
+}
 
 const content = computed({
   get: () => parseContent(props.modelValue),
@@ -250,6 +411,8 @@ const setHighlight = (color: string | null) => {
       :extensions="customExtensions"
       :handlers="customHandlers"
       :placeholder="editorConfig.placeholder.default"
+      :image="false"
+      :editor-props="{ transformPastedHTML, handlePaste }"
       class="editor-content"
     >
       <template #default="{ editor }">
